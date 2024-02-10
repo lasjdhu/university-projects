69ed2d72b5a0367971f38124c3e1c2ced55d8198
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2024-02-10 18:24:28 +0100

    feat: IAL

---
+++ IAL - Algorithms/graphs/main.c
@@ -0,0 +1,11 @@
+/*
+ * main.c
+ * Theme: Hamiltonian path and cycle in graph 
+ * Implementation: Oleksii Fedorchenko, Dmitrii Ivanushkin, Zlata Valakhanovich, December 2022
+ */
+#include "lib/graph.h"
+
+int main(int argc, char *argv[]) {
+    readFile(argc, argv);
+    return 0;
+}
