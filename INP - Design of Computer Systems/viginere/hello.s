de2336bf24fc3372b577a7f849f4772ce441113d
Dmitrii Ivanushkin <dmitrii.ivanushkin@protonmail.com>
2024-11-22 09:50:42 +0100

    feat(INP): viginere init (#5)

---
+++ INP - Design of Computer Systems/viginere/hello.s
@@ -0,0 +1,33 @@
+; Autor reseni: Dmitrii Ivanushkin xivanu00
+
+; Projekt 2 - INP 2024
+; Vigenerova sifra na architekture MIPS64
+
+; DATA SEGMENT
+                .data
+msg:            .asciiz "Hello world!" ; sem doplnte vase "jmenoprijmeni"
+cipher:         .space  31 ; misto pro zapis zasifrovaneho textu
+; zde si muzete nadefinovat vlastni promenne ci konstanty,
+; napr. hodnoty posuvu pro jednotlive znaky sifrovacho klice
+
+params_sys5:    .space  8 ; misto pro ulozeni adresy pocatku
+                          ; retezce pro vypis pomoci syscall 5
+                          ; (viz nize "funkce" print_string)
+
+; CODE SEGMENT
+                .text
+
+main:           ; ZDE NAHRADTE KOD VASIM RESENIM
+                daddi   r4, r0, msg ; vozrovy vypis: adresa msg do r4
+                jal     print_string ; vypis pomoci print_string - viz nize
+
+
+; NASLEDUJICI KOD NEMODIFIKUJTE!
+
+                syscall 0   ; halt
+
+print_string:   ; adresa retezce se ocekava v r4
+                sw      r4, params_sys5(r0)
+                daddi   r14, r0, params_sys5    ; adr pro syscall 5 musi do r14
+                syscall 5   ; systemova procedura - vypis retezce na terminal
+                jr      r31 ; return - r31 je urcen na return address
